97875d8087c2dcc61ddde862a5b27d4c7135b5f6
Robyn <r0797966@ucll.be>
2022-05-16 11:34:21 +0200

    first commit

---
+++ end.js
@@ -0,0 +1,36 @@
+const username = document.getElementById('username');
+const saveBtn = document.getElementById('saveBtn');
+const finalScore = document.getElementById('finalScore');
+
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+
+username.addEventListener('keyup', () => {
+    saveBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+
+    highScores.sort( (a, b) =>  b.score - a.score);
+
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    window.location.assign("/");
+
+    console.log(highScores)
+};
