cd32e64d2157fbefedbdb5a1fb99a8f765ea07c0
Robyn <r0797966@ucll.be>
2022-05-16 11:49:32 +0200

    comment end.js

--- end.js
+++ end.js
@@ -30,7 +30,7 @@ saveHighScore = (e) => {
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
 
-    window.location.assign("/QuizApp/home.html");
+    window.location.assign("/QuizApp/home.html"); // changed to fit github pages
 
     console.log(highScores)
 };
